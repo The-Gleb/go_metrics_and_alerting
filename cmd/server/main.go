335119cde4812fb61b94b3a20b3361657778d39e
Gleb <115395807+The-Gleb@users.noreply.github.com>
2024-03-09 20:03:34 +0300

    metricStorage constructor

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	// "bufio"
 	"context"
 	"log"
 	"net/http"
@@ -9,122 +8,29 @@ import (
 	"os/signal"
 	"sync"
 	"syscall"
-	// "time"
 
 	_ "net/http/pprof"
 
-	// "github.com/The-Gleb/go_metrics_and_alerting/internal/app"
-	// "github.com/The-Gleb/go_metrics_and_alerting/internal/compressor"
 	v1 "github.com/The-Gleb/go_metrics_and_alerting/internal/controller/http/v1/handler"
 	"github.com/The-Gleb/go_metrics_and_alerting/internal/controller/http/v1/middleware"
 	"github.com/The-Gleb/go_metrics_and_alerting/internal/domain/service"
 	"github.com/The-Gleb/go_metrics_and_alerting/internal/domain/usecase"
-	// "github.com/The-Gleb/go_metrics_and_alerting/internal/filestorage"
-	// "github.com/The-Gleb/go_metrics_and_alerting/internal/handlers"
 	"github.com/The-Gleb/go_metrics_and_alerting/internal/logger"
-	// "github.com/The-Gleb/go_metrics_and_alerting/internal/repositories"
 	"github.com/The-Gleb/go_metrics_and_alerting/internal/repository/database"
-	"github.com/The-Gleb/go_metrics_and_alerting/internal/repository/memory"
-	// "github.com/The-Gleb/go_metrics_and_alerting/internal/repository"
 	"github.com/The-Gleb/go_metrics_and_alerting/internal/repository/file_storage"
-	// "github.com/The-Gleb/go_metrics_and_alerting/internal/server"
-	// "github.com/The-Gleb/go_metrics_and_alerting/pkg/utils/retry"
+	"github.com/The-Gleb/go_metrics_and_alerting/internal/repository/memory"
+	postgresql "github.com/The-Gleb/go_metrics_and_alerting/pkg/client"
 	"github.com/go-chi/chi/v5"
 )
 
 // postgres://metric_db:metric_db@localhost:5434/metric_db?sslmode=disable
 
-// TODO: retries
 func main() {
 
 	if err := Run(); err != nil {
 		log.Fatal(err)
 	}
 
-	// go func() {
-	// 	log.Println(http.ListenAndServe("localhost:6060", nil))
-	// }()
-
-	// config := NewConfigFromFlags()
-
-	// if err := logger.Initialize(config.LogLevel); err != nil {
-	// 	logger.Log.Fatal(err)
-	// 	return
-	// }
-	// logger.Log.Info(config)
-
-	// var repository repositories.Repositiries
-	// var fileStorage app.FileStorage
-
-	// if config.FileStoragePath != "" {
-	// 	repository = memory.New()
-	// 	fileStorage = filestorage.NewFileStorage(config.FileStoragePath, config.StoreInterval, config.Restore)
-	// }
-
-	// if config.DatabaseDSN != "" {
-	// 	var db *database.DB
-	// 	var err error
-	// 	err = retry.DefaultRetry(
-	// 		context.Background(),
-	// 		func(ctx context.Context) error {
-	// 			db, err = database.ConnectDB(config.DatabaseDSN)
-	// 			return err
-	// 		},
-	// 	)
-
-	// 	if err != nil {
-	// 		logger.Log.Fatal(err)
-	// 		return
-	// 	}
-	// 	repository = db
-	// }
-
-	// app := app.NewApp(repository, fileStorage)
-	// handlers := handlers.New(app)
-	// s := server.NewWithProfiler(config.Addres, handlers, []byte(config.SignKey))
-
-	// if config.Restore {
-	// 	app.LoadDataFromFile(context.Background())
-	// }
-
-	// var wg sync.WaitGroup
-	// ctx, cancel := context.WithCancel(context.Background())
-
-	// if config.StoreInterval > 0 && config.DatabaseDSN == "" {
-	// saveTicker := time.NewTicker(time.Duration(config.StoreInterval) * time.Second)
-	// 	wg.Add(1)
-	// 	go func() {
-	// 		defer wg.Done()
-	// for {
-	// 	select {
-	// 	case <-saveTicker.C:
-	// 		app.StoreDataToFile(context.Background())
-	// 	case <-ctx.Done():
-	// 		logger.Log.Debug("stop saving to file")
-	// 		return
-	// 	}
-	// }
-
-	// 	}()
-	// }
-
-	// wg.Add(1)
-	// go func() {
-	// 	defer wg.Done()
-	// 	ServerShutdownSignal := make(chan os.Signal, 1)
-	// 	signal.Notify(ServerShutdownSignal, syscall.SIGINT)
-	// 	<-ServerShutdownSignal
-	// 	s.Shutdown(context.Background())
-	// 	cancel()
-	// }()
-
-	// err := server.Run(s)
-	// if err != nil && err != http.ErrServerClosed {
-	// 	panic(err)
-	// }
-	// os.
-	// 	wg.Wait()
-	// logger.Log.Info("server shutdown")
 }
 
 func Run() error {
@@ -149,8 +55,11 @@ func Run() error {
 	}
 
 	if config.DatabaseDSN != "" {
-
-		db, err := database.ConnectDB(config.DatabaseDSN)
+		client, err := postgresql.NewClient(context.Background(), config.DatabaseDSN)
+		if err != nil {
+			return err
+		}
+		db, err := database.NewMetricDB(client)
 		if err != nil {
 			return err
 		}
